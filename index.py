2028571ac261e8f6e3f24bf089ada99943c2d7e8
Pollyana <pollyanacassia8@gmail.com>
2023-10-23 02:14:19 -0300

    Exibir uma lista de todos os registros de estudantes

--- index.py
+++ index.py
@@ -7,11 +7,9 @@ contador = 0
 saudacao = "Olá, seja bem vindo(a)!"
 print(saudacao)
 
-
 while atendimento == True or outroAtendimento == "sim":
 
-  atendimento = input("Por favor digite a opção desejada: \n 1 - Cadastro de estudante \n 2 - Consutar registros de estudantes \n 3 - Consultar estudante pelo seu ID \n 4 - Consultar média de notas de todos os estudantes \n 5 - Salvar registros de estudantes em um arquivo \n 6 - Carregar os registros de estudantes de um arquivo \n 7 - Sair \n")
-
+  atendimento = input("Por favor digite a opção desejada: \n 1 - Cadastro de estudante; \n 2 - Consutar registros de estudantes; \n 3 - Consultar estudante pelo seu ID; \n 4 - Consultar média de notas de todos os estudantes; \n 5 - Salvar registros de estudantes em um arquivo; \n 6 - Carregar os registros de estudantes de um arquivo; \n 7 - Sair. \n")
   
   if atendimento == "1": 
      contador += 1
@@ -31,11 +29,17 @@ while atendimento == True or outroAtendimento == "sim":
 
        if resposta == "nao":
          print("Estudante cadastrado com sucesso!") 
-         outroAtendimento = input("Deseja outro atendimento? ")
+         outroAtendimento = input("Deseja outro atendimento, digite sim ou nao? ")
          print(outroAtendimento)
         
+        # 2 - Exibir uma lista de todos os registros de estudantes.
   elif atendimento == "2":
-    print("opção 2")
+    print(f"A lista de todos os registros de estudantes cadastrados é\n{lista}")
+    resposta = input("Deseja continuar, digite sim ou nao: ") 
+    if resposta == "nao":
+        outroAtendimento = input("Deseja outro atendimento, digite sim ou nao? ")
+        print(outroAtendimento)
+
   elif atendimento == "3":
     print("opção 3")
   elif atendimento == "4":
